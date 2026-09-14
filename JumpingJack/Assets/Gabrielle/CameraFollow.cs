dffc5a77221c49a5277071f3791498a3ae135689
funkipunkyx <gabriellejimenezmiranda@gmail.com>
2026-09-14 14:22:22 -0600

    camera scrpt b4 changes

---
+++ JumpingJack/Assets/Gabrielle/CameraFollow.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    void LateUpdate ()
+    {
+        if(target.position.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = newPos;
+        }    
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
