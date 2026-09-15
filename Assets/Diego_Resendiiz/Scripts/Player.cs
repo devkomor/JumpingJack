5e296dc9e32f2bc7d5e04fa8172ad7610b11f9d4
Depower07 <resendizdiegog@gmail.com>
2026-09-15 12:40:35 -0600

    Player script

---
+++ Assets/Diego_Resendiiz/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float MovementSpeed = 5f; 
+
+    private float _movementValue = 0f; 
+    private Rigidbody2D _playerRigidbody; 
+
+    void Start() 
+    {
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() 
+    {
+        _movementValue = Input.GetAxis("Horizontal") * MovementSpeed;
+    }
+
+    void FixedUpdate() 
+    {
+        Vector2 currentVelocity = _playerRigidbody.linearVelocity;
+        currentVelocity.x = _movementValue;
+        _playerRigidbody.linearVelocity = currentVelocity;
+    }
+}
